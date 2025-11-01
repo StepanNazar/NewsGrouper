d81eb007f2ebbb911c3077a55949521c9cb165cb
Nazar Stepan <64284448+StepanNazar@users.noreply.github.com>
2025-11-01 21:24:06 +0200

    test: refactor profile tests to use parametrize

--- tests/api/test_profiles.py
+++ tests/api/test_profiles.py
@@ -1,192 +1,187 @@
 import time
 import uuid
+from types import MappingProxyType
 
 import pytest
 
-
-@pytest.fixture
-def profile_data():
-    return {
+profile_data = MappingProxyType(
+    {  # immutable dict view to ensure test isolation
         "name": "Test Profile",
         "description": "This is a test profile",
     }
+)
+
+
+def create_profile(client, profile_data: dict | MappingProxyType = profile_data):
+    response = client.post("/api/profiles", json=profile_data.copy())
+    return response.headers["Location"]
 
 
 @pytest.fixture
-def post_profile(authenticated_client, profile_data):
-    response = authenticated_client.post("/api/profiles", json=profile_data)
-    return response
+def profile(authenticated_client):
+    """Sets up a test case with a profile with profile_data"""
+    return create_profile(authenticated_client)
 
 
-def test_post_profile(authenticated_client, profile_data):
-    response = authenticated_client.post("/api/profiles", json=profile_data)
+@pytest.fixture
+def cross_user_profiles(authenticated_client, authenticated_client2):
+    """Creates profiles for two different users."""
+    return (create_profile(authenticated_client), create_profile(authenticated_client2))
+
+
+def test_post_profile(authenticated_client):
+    response = authenticated_client.post("/api/profiles", json=profile_data.copy())
 
     assert response.status_code == 201
     assert "Location" in response.headers
-    assert_profile_response(response, profile_data)
+    assert_response_matches_profile(response, profile_data)
 
 
-def test_post_profile_unauthorized(client, profile_data):
-    response = client.post("/api/profiles", json=profile_data)
+def test_get_profile(authenticated_client, profile):
+    response = authenticated_client.get(profile)
+
+    assert response.status_code == 200
+    assert_response_matches_profile(response, profile_data)
+
+
+def test_put_profile(authenticated_client, profile):
+    updated_profile = profile_data.copy()
+    updated_profile["name"] = "Updated Profile"
+
+    time.sleep(0.01)  # Ensure the updated timestamp is different
+    response = authenticated_client.put(profile, json=updated_profile)
+
+    assert response.status_code == 200
+    assert_response_matches_profile(response, updated_profile)
+    assert response.json["updated"] != response.json["created"]
+
+
+def test_delete_profile(authenticated_client, profile):
+    response = authenticated_client.delete(profile)
+
+    assert response.status_code == 204
+
+    response = authenticated_client.get(profile)
+
+    assert response.status_code == 404
+
+
+def test_get_profiles(authenticated_client):
+    profiles = []
+    for i in range(3):
+        new_profile = profile_data.copy()
+        new_profile["name"] = f"Profile {i + 1}"
+        profiles.append(new_profile)
+    for new_profile in profiles:
+        create_profile(authenticated_client, new_profile)
+
+    response = authenticated_client.get("/api/profiles?order=asc")
+
+    assert response.status_code == 200
+    assert "items" in response.json
+    assert len(response.json["items"]) == 3
+    for i, expected_profile in enumerate(profiles):
+        returned_profile = response.json["items"][i]
+        for key, value in expected_profile.items():
+            assert returned_profile[key] == value
+
+
+@pytest.mark.parametrize(
+    "method,payload",
+    [
+        ("get", None),
+        ("put", {"name": "Updated", "description": "Updated"}),
+        ("delete", None),
+    ],
+)
+def test_access_unowned_profile(
+    authenticated_client, authenticated_client2, cross_user_profiles, method, payload
+):
+    client_profile, client2_profile = cross_user_profiles
+
+    assert_clients_cant_access_unowned_profiles(
+        authenticated_client,
+        authenticated_client2,
+        client_profile,
+        client2_profile,
+        method,
+        payload,
+    )
+
+    if method != "get":
+        assert_profile_unchanged(authenticated_client, client_profile, profile_data)
+        assert_profile_unchanged(authenticated_client2, client2_profile, profile_data)
+
+
+@pytest.mark.parametrize(
+    "method,url,payload",
+    [
+        ("post", "/api/profiles", profile_data.copy()),
+        ("get", "/api/profiles/1", None),
+        ("put", "/api/profiles/1", profile_data.copy()),
+        ("delete", "/api/profiles/1", None),
+        ("get", "/api/profiles", None),
+    ],
+)
+def test_unauthorized_access(client, method, url, payload):
+    response = getattr(client, method)(url, json=payload)
 
     assert response.status_code == 401
 
 
-def test_get_profile(authenticated_client, profile_data):
-    location = create_profile(authenticated_client, profile_data)
+@pytest.mark.parametrize(
+    "method,payload",
+    [
+        ("get", None),
+        ("put", profile_data.copy()),
+        ("delete", None),
+    ],
+)
+def test_profile_not_found(authenticated_client, method, payload):
+    # create a profile to check it won't be returned
+    profile_url = create_profile(authenticated_client)
+    prefix, id = profile_url.rsplit("/", maxsplit=1)
+    non_existent_profile_url = f"{prefix}/{int(id) + 1}"
 
-    response = authenticated_client.get(location)
+    response = getattr(authenticated_client, method)(
+        non_existent_profile_url, json=payload
+    )
+    assert response.status_code == 404
 
-    assert response.status_code == 200
-    assert_profile_response(response, profile_data)
 
-
-def test_get_profile_not_found(authenticated_client, post_profile):
-    non_existent_id = random_id()
+def test_get_with_huge_id(authenticated_client):
     response = authenticated_client.get(
-        f"/api/profiles/{non_existent_id}"
+        f"/api/profiles/{int(uuid.uuid4().hex, 16)}"
     )  # breaks sqlite
 
     assert response.status_code == 404
 
 
-def test_get_profile_unauthorized(client):
-    response = client.get("/api/profiles/1")
-
-    assert response.status_code == 401
-
-
-def test_get_unowned_profile(authenticated_client, authenticated_client2, profile_data):
-    location = create_profile(authenticated_client, profile_data)
-    location2 = create_profile(authenticated_client2, profile_data)
-
-    response = authenticated_client2.get(location)
-    response2 = authenticated_client.get(location2)
-
-    assert response.status_code == 404
-    assert response2.status_code == 404
-
-
-def test_put_profile(authenticated_client, profile_data):
-    location = create_profile(authenticated_client, profile_data)
-    updated_profile = profile_data.copy()
-    updated_profile["name"] = "Updated Profile"
-
-    time.sleep(0.01)  # Ensure updated timestamp is different
-    response = authenticated_client.put(location, json=updated_profile)
-
+def assert_profile_unchanged(client, url, expected_data):
+    """Verify profile wasn't modified."""
+    response = client.get(url)
     assert response.status_code == 200
-    assert_profile_response(response, updated_profile)
-    assert response.json["updated"] != response.json["created"]
+    assert_response_matches_profile(response, expected_data)
 
 
-def test_put_profile_not_found(authenticated_client, post_profile):
-    non_existent_id = random_id()
-    response = authenticated_client.put(f"/api/profiles/{non_existent_id}")
-
-    assert response.status_code == 404
-
-
-def test_put_profile_unauthorized(client, profile_data):
-    response = client.put("/api/profiles/1", json=profile_data)
-
-    assert response.status_code == 401
-
-
-def test_put_unowned_profile(authenticated_client, authenticated_client2, profile_data):
-    location = create_profile(authenticated_client, profile_data)
-    location2 = create_profile(authenticated_client2, profile_data)
-    updated_profile = profile_data.copy()
-    updated_profile["name"] = "Updated Profile"
-
-    response = authenticated_client2.put(location, json=updated_profile)
-    response2 = authenticated_client.put(location2, json=updated_profile)
-
-    assert response.status_code == 404
-    assert response2.status_code == 404
-
-    response = authenticated_client.get(location)
-    response2 = authenticated_client2.get(location2)
-
-    assert_profile_response(response, profile_data)
-    assert_profile_response(response2, profile_data)
-
-
-def test_delete_profile(authenticated_client, profile_data):
-    location = create_profile(authenticated_client, profile_data)
-
-    response = authenticated_client.delete(location)
-
-    assert response.status_code == 204
-
-    response = authenticated_client.get(location)
-    assert response.status_code == 404
-
-
-def test_delete_profile_not_found(authenticated_client, post_profile):
-    not_existent_id = random_id()
-    response = authenticated_client.delete(f"/api/profiles/{not_existent_id}")
-
-    assert response.status_code == 404
-
-
-def test_delete_profile_unauthorized(client):
-    response = client.delete("/api/profiles/1")
-
-    assert response.status_code == 401
-
-
-def test_delete_unowned_profile(
-    authenticated_client, authenticated_client2, profile_data
+def assert_clients_cant_access_unowned_profiles(
+    authenticated_client,
+    authenticated_client2,
+    client_profile,
+    client2_profile,
+    method,
+    payload=None,
 ):
-    location = create_profile(authenticated_client, profile_data)
-    location2 = create_profile(authenticated_client2, profile_data)
-
-    response = authenticated_client2.delete(location)
-    response2 = authenticated_client.delete(location2)
+    response = getattr(authenticated_client2, method)(client_profile, json=payload)
+    response2 = getattr(authenticated_client, method)(client2_profile, json=payload)
 
     assert response.status_code == 404
     assert response2.status_code == 404
 
-    response = authenticated_client.get(location)
-    response2 = authenticated_client2.get(location2)
 
-    assert response.status_code == 200
-    assert response2.status_code == 200
-
-
-def test_get_profiles(authenticated_client, profile_data):
-    for i in range(3):
-        new_profile = profile_data.copy()
-        new_profile["name"] = f"Profile {i + 1}"
-        authenticated_client.post("/api/profiles", json=new_profile)
-
-    response = authenticated_client.get("/api/profiles")
-
-    assert response.status_code == 200
-    assert "items" in response.json
-    assert len(response.json["items"]) == 3
-
-
-def test_get_profiles_unauthorized(client):
-    response = client.get("/api/profiles")
-
-    assert response.status_code == 401
-
-
-def create_profile(client, profile_data):
-    response = client.post("/api/profiles", json=profile_data)
-    return response.headers["Location"]
-
-
-def assert_profile_response(response, profile):
+def assert_response_matches_profile(response, profile):
     for key, value in profile.items():
         assert response.json[key] == value
     assert "id" in response.json
     assert "created" in response.json
     assert "updated" in response.json
-
-
-def random_id() -> int:
-    non_existent_id = int(uuid.uuid4().hex, 16)
-    return non_existent_id
